b4afee86e01ce77b5f8c6b8f5feceefbef22a4eb
Francis Campos <francis.goes@yahoo.com.br>
2024-06-18 21:48:21 -0300

    Initial commit: Added base code for real-time video capture and display using Java, OpenCV, and JavaCV.

---
+++ src/reconhecimento/testeJavaCV.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package reconhecimento;
+
+
+
+import org.bytedeco.opencv.opencv_face.*; 
+
+
+/**
+ *
+ * @author 38586286893
+ */
+public class testeJavaCV {
+    
+    
+    public static void main(String args[] ){
+       
+        
+        FaceRecognizer r = EigenFaceRecognizer.create();
+        
+    }
+    
+}
